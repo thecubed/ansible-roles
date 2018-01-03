1efdac768403d4186f308233206e2fee74f472f1
Tyler Montgomery <tyler@tyler-mbp.local>
2018-01-03 04:43:55 -0800

    Initial commit for codefire buildbox templates

---
+++ roles/builder/files/etc/profile.d/codefire_mksymlinks.sh
@@ -0,0 +1,32 @@
+STORAGE_DIR="/mnt/nfs-fs1-nfshomes_storage"
+DOWNLOADS_DIR="/mnt/datastore/downloads"
+SKIP_FILE="/home/$USER/.skipsymlinks"
+
+
+if [ -f $SKIP_FILE ]
+then   
+        return
+        #echo "[bashrc] Not skipping link creation..."
+fi
+
+if [ ! -L ~/storage ]
+then   
+        echo "[bashrc] No storage symlink found! Creating one..."
+        echo "[bashrc] Linking $STORAGE_DIR/$USER to ~/storage"
+        ln -s $STORAGE_DIR/$USER ~/storage
+        touch $SKIP_FILE
+fi
+
+if [ ! -L ~/downloads ]
+then   
+        echo "[bashrc] No downloads symlink found! Creating one..."
+        echo "[bashrc] Linking $DOWNLOADS_DIR/$USER to ~/downloads"
+        ln -s $DOWNLOADS_DIR/$USER ~/downloads
+        touch $SKIP_FILE
+fi
+
+if [ ! -d ~/bin ]
+then   
+        echo "[bashrc] Creating ~/bin folder..."
+        mkdir ~/bin
+fi
